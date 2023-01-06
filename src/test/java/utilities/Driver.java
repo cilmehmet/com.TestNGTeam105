23bce6fa0fe3e76e135b378044b38181a1aa097a
Mehmetcil <mehmetcil096@gmail.com>
2023-01-06 23:16:55 +0300

    day16

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,24 +3,58 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class Driver {
 
+    /*
+    Driver class'ından driver'i gerDriver() ile kullanıyoruz
+    Sonradan projeye katılan insanların driver class'ından obje olusturarak
+    driver kullanmaya calışmalarını engellemek için
+    Driver class'ını SINGLETON PATERN ile düzenleyebiliriz
+
+    Bunun için Driver class'ının parametresiz Constructer'ını olusturup
+    access modifier'ını PRIVATE yapmamız yeterli olur
+     */
+
     static WebDriver driver;
 
     public static WebDriver getDriver(){
 
+        String browser= ConfigReader.getProperty("browser");
         if(driver==null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            switch (browser) {
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver= new SafariDriver();
+                    break;
+
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+
+            }
+
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
-
         return driver;
-
     }
 
     public static void closeDriver(){
